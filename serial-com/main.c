cbb38809710bd7a88dfa8097cd0fffaa2f2eedc3
zhongsp <patrick.zhongsp@gmail.com>
2022-06-24 14:16:36 +0800

    wip

--- serial-com/main.c
+++ serial-com/main.c
@@ -43,8 +43,13 @@ void uart() interrupt 4
 {
     unsigned char rec_data;
 
+    // 必须软件置 0
     RI = 0;
+    
+    // 读取接收到的数据
     rec_data = SBUF;
+    
+    // 写待发送数据，单片机自动发送
     SBUF = rec_data;
     while (!TI)
         ;
